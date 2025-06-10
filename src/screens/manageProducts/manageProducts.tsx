5660a4c0098e2a4376e82c5bce94ceb931ca7728
Nicolás Willemet <nwillemet18@gmail.com>
2025-06-10 18:56:48 -0300

    Subida sin conflictos

--- src/screens/manageProducts/manageProducts.tsx
+++ src/screens/manageProducts/manageProducts.tsx
@@ -25,14 +25,6 @@ export default function ManageProducts() {
     }[]
   >([]);
 
-
-  const [filtros, setFiltros] = useState({
-    genero: '',
-    categoriaId: null as number | null,
-    talle: '' as number | '',
-    orden: '',
-  });
-  
   useEffect(() => {
     const fetchProductos = async () => {
       try {
@@ -44,7 +36,7 @@ export default function ManageProducts() {
           gender: p.genero,
           price: `$${p.precio}`,
           stock: Math.floor(Math.random() * 21), // stock aleatorio entre 0 y 20
-          image: imgTable, // imagen fija para todos
+          image: p.imagen ||imgTable, 
         }));
 
         setProducts(mappedProducts);
@@ -103,7 +95,7 @@ export default function ManageProducts() {
             !showFilters ? styles.hideFilter : ""
           }`}
         >
-            <Filter filtros={filtros} setFiltros={setFiltros} />
+          <Filter />
         </div>
         <div
           className={`${styles.tableSection} ${
